cc5048402e4385298704fa1a45c52aa30f2370e2
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2020-05-28 15:04:33 -0300

    :lipstick: Making it responsive and adjusting some details

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -3,22 +3,17 @@ import styled from 'styled-components'
 export const Wrapper = styled.header`
   background: #15092D;
   width: 100%;
-  height: 9vh;
   height: 53px;
   display: flex;
   align-items: center;
-  margin: 5px;
-  width: 29px;
-  height: 31px;
-
-  :last-of-type {
-    margin-right: 20px;
+   
+  svg{
+    cursor: pointer;
+    margin: 5px;
+    width: 29px;
+    height: 31px;
   }
-  :first-of-type {
-    margin-left: 20px;
-  }   
-}
-  
+
   @media (max-width: 800px){
     width: 100vw;
     display: flex;
@@ -31,7 +26,6 @@ export const SvgButtons = styled.div`
   margin-left: auto;
 `
 
-export const ButtonPreview = styled.button``
 
 export const ButtonPreview = styled.button`
   display:none;
@@ -50,7 +44,7 @@ export const ButtonPreview = styled.button`
     border-radius: 3px;
    
     color: #EDECEC;
-    font-size: 1.2em;
+    font-size: 1em;
     font-weight: bold;
   }
 `
